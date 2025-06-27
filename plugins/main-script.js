3a63acf7df9bc1e174cb157d28605bf554fa5c1b
nene504273 <salassantos675@gmail.com>
2025-06-27 07:22:02 -0400

    Actualizar main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
 try {
-let res = await fetch('https://api.github.com/repos/Dioneibi-rip/Ruby-Hoshino-Bot')
+let res = await fetch('https://api.github.com/repos/nene504273/Monkey-D-luffy-Bot-MD')
 
 if (!res.ok) throw new Error('Error al obtener datos del repositorio')
 let json = await res.json()
