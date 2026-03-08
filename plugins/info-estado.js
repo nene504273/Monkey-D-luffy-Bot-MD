f7786ba550ef3edc43551ed570af6e1ffb208d20
DuarteXV <danielalejandrobasado@gmail.com>
2026-03-08 09:09:39 -0400

    Actualizar info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -1,22 +1,21 @@
 import ws from 'ws'
 
-let handler = async (m, { conn, usedPrefix, isRowner }) => {
-  let _uptime = process.uptime() * 1000;
+let handler = async (m, { conn, usedPrefix }) => {
+  let _uptime = process.uptime() * 1000
   let totalreg = Object.keys(global.db.data.users).length
-  let totalchats = Object.keys(global.db.data.chats).length
-  let uptime = clockString(_uptime);
-  let users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
+  let uptime = clockString(_uptime)
+  let users = global.conns.filter(c => c.user && c.ws.socket && c.ws.socket.readyState !== ws.CLOSED)
   const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
   const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
-  const totalUsers = users.length;
+  const totalUsers = users.length
   let old = performance.now()
   let neww = performance.now()
   let speed = neww - old
 
-  let info = `🔥 *¡Hola, soy Luffy!* 🔥\n`
+  let info = `🔥 *¡Hola, soy ${global.botname}!* 🔥\n`
   info += `👑 *Creador* ⇢ ɴ͡ᴇ͜ɴᴇ❀᭄☂️\n`
   info += `💫 *Prefijo* ⇢ [ ${usedPrefix} ]\n`
-  info += `🚣‍♂️ *Versión* ⇢ ${vs}\n`
+  info += `🚣‍♂️ *Versión* ⇢ ${global.vs}\n`
   info += `📊 *Chats Privados* ⇢ ${chats.length - groupsIn.length}\n`
   info += `📈 *Total De Chats* ⇢ ${chats.length}\n`
   info += `👥 *Usuarios* ⇢ ${totalreg}\n`
@@ -26,7 +25,7 @@ let handler = async (m, { conn, usedPrefix, isRowner }) => {
   info += `🤖 *Sub-Bots Activos* ⇢ ${totalUsers || '0'}`
 
   await conn.sendMessage(m.chat, {
-    image: { url: 'https://files.catbox.moe/uw0lmt.jpg' },
+    image: { url: 'https://raw.githubusercontent.com/danielalejandrobasado-glitch/Yotsuba-MD-Premium/main/uploads/c7f472a445daffd7.jpg' },
     caption: info
   }, { quoted: m })
 }
@@ -34,13 +33,12 @@ let handler = async (m, { conn, usedPrefix, isRowner }) => {
 handler.help = ['estado']
 handler.tags = ['info']
 handler.command = ['estado', 'status', 'estate', 'state', 'stado', 'stats']
-handler.register = true
 
 export default handler
 
 function clockString(ms) {
-  let seconds = Math.floor((ms / 1000) % 60);
-  let minutes = Math.floor((ms / (1000 * 60)) % 60);
-  let hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
-  return `${hours}h ${minutes}m ${seconds}s`;
+  let seconds = Math.floor((ms / 1000) % 60)
+  let minutes = Math.floor((ms / (1000 * 60)) % 60)
+  let hours = Math.floor((ms / (1000 * 60 * 60)) % 24)
+  return `${hours}h ${minutes}m ${seconds}s`
 }
