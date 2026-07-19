5f2f85041fdcea7e99cc8ca08dbe4ce5b60b0e4e
ɴ͡ᴇ͜ɴᴇ❀᭄☂️ <salassantos675@gmail.com>
2026-07-19 10:42:43 -0400

    Actualizar ping.js

--- cmds/info/ping.js
+++ cmds/info/ping.js
@@ -8,7 +8,7 @@ export default {
     const latency = Date.now() - start
 
     await sock.sendMessage(msg.chat, {
-      text: `✿ *Pong!*\n> Tiempo ⴵ ${latency}ms`,
+      text: `⚡ *Pong!*\n> Latencia ⴵ ${latency}ms`,
       edit: sent.key
     }, { quoted: msg })
   },
