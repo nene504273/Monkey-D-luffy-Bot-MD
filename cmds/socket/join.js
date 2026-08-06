5df6a276a275c76bf1978f6c0b0757648abd2fea
ɴ͡ᴇ͜ɴᴇ❀᭄☂️ <salassantos675@gmail.com>
2026-08-06 09:15:55 -0400

    Actualizar join.js

--- cmds/socket/join.js
+++ cmds/socket/join.js
@@ -1,29 +1,18 @@
-import db from "#db"
-
+Import db from "#db"
 export default {
   command: ['join', 'unir'],
   isModeration: true,
   run: async ({ msg, sock, args }) => {
     const idBot = sock.user.id.split(':')[0] + '@s.whatsapp.net'
     const config = await db.getSettings(idBot)
-    
-    const ownerList = (global.owner || []).map(num => `${num}@s.whatsapp.net`)
-    
-    const isOwner2 = [
-      idBot, 
-      ...ownerList, 
-      ...(global.mods || []).map(num => num.includes('@') ? num : `${num}@s.whatsapp.net`)
-    ].includes(msg.sender)
-    
+    const isOwner2 = [idBot, ...(config.owner ? [config.owner] : []), ...global.mods.map(num => num + '@s.whatsapp.net')].includes(msg.sender)
     if (!isOwner2) return msg.reply(mess.socket)
-    
     if (!args[0]) return msg.reply('《✧》 Ingresa el enlace del grupo para unir el bot.')
-    
     const linkRegex = /chat\.whatsapp\.com\/([0-9A-Za-z]{20,24})/i
     const match = args[0].match(linkRegex)
-    
-    if (!match || !match[1]) return msg.reply('《✧》 El enlace ingresado no es válido o está incompleto.')
-    
+    if (!match || !match[1]) {
+      return msg.reply('《✧》 El enlace ingresado no es válido o está incompleto.')
+    }
     try {
       const inviteCode = match[1]
       await sock.groupAcceptInvite(inviteCode)
@@ -40,3 +29,5 @@ export default {
     }
   },
 }
+
+
